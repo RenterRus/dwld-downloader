0fb877e365a9d0e65c01f0f8811c938d62b87fe5
Artur Davydov <a.davydov@zdravcity.ru>
2025-06-21 21:34:35 +0300

    init: add http healt-check

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,31 @@
 package app
 
+import (
+	"dwld-downloader/internal/controller/http"
+	"fmt"
+	"sync"
+)
+
 func NewApp() error {
-	ReadConfig(".", "config_example")
+	conf, err := ReadConfig(".", "config_example")
+	if err != nil {
+		return fmt.Errorf("ReadConfig: %w", err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		http.NewHttpServer(&http.Server{
+			Host:   conf.HTTP.Host,
+			Port:   conf.HTTP.Port,
+			Enable: conf.HTTP.Enable,
+		})
+	}()
+
+	wg.Wait()
 
 	return nil
 }
--- internal/controller/http/router.go
+++ internal/controller/http/router.go
@@ -1,1 +1,30 @@
 package http
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	Host   string
+	Port   int
+	Enable bool
+}
+
+func NewHttpServer(s *Server) error {
+	if s.Enable {
+		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(fmt.Sprintf("Available at %s", time.Now().Format(time.DateTime))))
+		})
+
+		fmt.Printf("HTTP starting in %s:%d (/health)", s.Host, s.Port)
+
+		if err := http.ListenAndServe(fmt.Sprintf("%s:%d", s.Host, s.Port), nil); err != nil {
+			return fmt.Errorf("http server is stopped: %w", err)
+		}
+	}
+
+	return fmt.Errorf("http server not started: disabled in config")
+}
