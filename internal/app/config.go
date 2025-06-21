c54c85347e4448883dae3a52c249921c179d8d1d
Artur Davydov <a.davydov@zdravcity.ru>
2025-06-21 20:42:47 +0300

    init: added config struct

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,1 +1,25 @@
 package app
+
+type Server struct {
+	Host   string
+	Port   int
+	Enable bool
+}
+
+type FTPClient struct {
+	Addr            Server
+	User            string
+	Pass            string
+	RemoteDirectory string
+}
+
+type Config struct {
+	GRPC Server
+	HTTP Server
+	FTP  FTPClient
+
+	PathToDB string
+	Cache    Server
+
+	EagerMode bool
+}
