93e2ad2ec92aac22cbebf18893f4d8b4122bae43
Artur Davydov <a.davydov@zdravcity.ru>
2025-07-08 00:22:15 +0300

    init: downloader

--- pkg/downloader/downloader.go
+++ pkg/downloader/downloader.go
@@ -1,1 +1,50 @@
 package downloader
+
+import (
+	"context"
+	"dwld-downloader/internal/controller/downloader"
+	"dwld-downloader/internal/repo/persistent"
+	"dwld-downloader/internal/repo/temporary"
+)
+
+type DownloaderConf struct {
+	WorkDir       string
+	Threads       int
+	PercentToNext int
+	Stages        []downloader.Stage
+	SqlRepo       persistent.SQLRepo
+	Cache         temporary.CacheRepo
+}
+
+type Downloader struct {
+	downloader downloader.Downloader
+	notify     chan struct{}
+}
+
+func NewDownloader(conf DownloaderConf) *Downloader {
+	return &Downloader{
+		downloader: downloader.NewDownloader(downloader.DownloaderConf{
+			WorkDir:       conf.WorkDir,
+			Threads:       conf.Threads,
+			PercentToNext: conf.PercentToNext,
+			Stages:        conf.Stages,
+			SqlRepo:       conf.SqlRepo,
+			Cache:         conf.Cache,
+		}),
+		notify: make(chan struct{}, 1),
+	}
+}
+
+func (d *Downloader) Start() {
+	ctx, cncl := context.WithCancel(context.Background())
+	go func() {
+		d.downloader.Processor(ctx)
+	}()
+
+	<-d.notify
+	cncl()
+}
+
+func (d *Downloader) Stop() {
+	d.notify <- struct{}{}
+}
